a7738a22568e1f7933e03dc8c777e75061491fa0
Arun <arunkumar280491@gmail.com>
2018-05-14 16:21:54 +0530

    split the files

--- urlload.py
+++ urlload.py
@@ -1,5 +1,6 @@
 
 # load the url content
+# https://stackoverflow.com/questions/17178483/how-do-you-send-an-http-get-web-request-in-python
 from urllib.request import urlopen
 html = urlopen("http://www.google.com/").read()
 print(html)
