34054c870784b95c37864d3614e9f29360669d70
Arun <arunkumar280491@gmail.com>
2018-05-14 16:02:38 +0530

    http call & jsonencode/decode for loop

--- pyth.py
+++ pyth.py
@@ -1,8 +1,7 @@
-# load the url
-# from urllib.request import urlopen
-# html = urlopen("http://www.google.com/").read()
-# print(html)
 
+# ref https://www.journaldev.com/19213/python-http-client-request-get-post
+# http://www.pythonforbeginners.com/loops/for-while-and-nested-loops-in-python
+# http://developer.rhino3d.com/guides/rhinopython/python-xml-json/
 import http.client
 import json
 conn = http.client.HTTPConnection("jsonplaceholder.typicode.com")
@@ -17,12 +16,17 @@ for m in j:
 
 
 
+# load the url content
+from urllib.request import urlopen
+html = urlopen("http://www.google.com/").read()
+print(html)
 
-# comment the code
-# a, b, c = 1, 2, "john"
-# print (c)
-# k = True
-# k= False
-# if k:
-#     print (k)
-# print("arun")
+
+# basic variable declaration
+a, b, c = 1, 2, "john"
+print (c)
+k = True
+k= False
+if k:
+    print (k)
+print("arun")
