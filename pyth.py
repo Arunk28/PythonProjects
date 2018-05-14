fe4261dc518744ae7658f8dae91d019d3af6a8dd
Arun <arunkumar280491@gmail.com>
2018-05-14 14:58:52 +0530

    http call

--- pyth.py
+++ pyth.py
@@ -1,7 +1,18 @@
 # load the url
-from urllib.request import urlopen
-html = urlopen("http://www.google.com/").read()
-print(html)
+# from urllib.request import urlopen
+# html = urlopen("http://www.google.com/").read()
+# print(html)
+
+import http.client
+conn = http.client.HTTPConnection("api.github.com")
+headers = {"Content-type":"application/json","UserAgent ":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"}
+conn.request("GET", "/meta", "", headers)
+
+res = conn.getresponse()
+print(res.status)
+
+
+
 
 # comment the code
 # a, b, c = 1, 2, "john"
